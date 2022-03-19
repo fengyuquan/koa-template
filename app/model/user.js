b45e8124d3001dbc163d63120dd241723f90f193
fengyuquan <420125423@qq.com>
2022-03-19 20:29:57 +0800

    feat：sequelize ORM使用示范

---
+++ app/model/user.js
@@ -0,0 +1,36 @@
+const bcrypt = require('bcryptjs')
+const {DataTypes, Model} = require('sequelize')
+const sequelize = require('../../core/db')
+
+class User extends Model {
+}
+
+User.init({
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true, // 主键
+        autoIncrement: true, // 自动增长
+    },
+    nickname: {
+        type: DataTypes.STRING,
+    },
+    email: {
+        type: DataTypes.STRING(128),
+        unique: true, // 唯一性
+        allowNull: false,
+    },
+    password: {
+        type: DataTypes.STRING,
+        set(val) {
+            const salt = bcrypt.genSaltSync(10)
+            const pwd = bcrypt.hashSync(val, salt)
+            this.setDataValue('password', pwd)
+        },
+        allowNull: false,
+    },
+}, {
+    sequelize, // 需要传递连接实例
+    tableName: 'user', // 需要选择模型名称
+})
+
+module.exports = User
--- config/config.js
+++ config/config.js
@@ -4,4 +4,11 @@ module.exports = {
     env: 'dev',
     port: '3000',
     logPath: path.resolve(__dirname, '../logs/koa-template.log'),
+    database: {
+        dbName: 'koa-template',
+        host: 'localhost',
+        port: 3306,
+        username: 'root',
+        password: ''
+    },
 }
